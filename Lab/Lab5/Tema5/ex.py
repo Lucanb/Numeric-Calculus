bd2daa3e1ad9e697e425a075974ea92ba9d1631d
Lucanb <lucanastasa@.com>
2024-04-04 12:56:59 +0300

    done

--- Lab/Lab5/Tema5/ex.py
+++ Lab/Lab5/Tema5/ex.py
@@ -1,21 +1,20 @@
 import numpy as np
 
-def jacobi(A, epsilon=1e-10, max_iter=1000):
+def jacobiDivision(A, eps=1e-10, K_max=1000):
     n = A.shape[0]
     U = np.eye(n) 
     k = 0
     
-    while k < max_iter:
-
-        max_elem = 0
+    while k < K_max:
+        maximum = 0
         p, q = 0, 0
         for i in range(n):
             for j in range(i+1, n):
-                if abs(A[i, j]) > max_elem:
-                    max_elem = abs(A[i, j])
+                if abs(A[i, j]) > maximum:
+                    maximum = abs(A[i, j])
                     p, q = i, j
         
-        if max_elem < epsilon:
+        if maximum < eps:
             break  
 
         alpha = (A[p, p] - A[q, q]) / (2 * A[p, q])
@@ -24,36 +23,27 @@ def jacobi(A, epsilon=1e-10, max_iter=1000):
         else:
             t = -alpha - np.sqrt(alpha**2 + 1)
         
-        c = 1 / np.sqrt(1 + t**2)
-        s = c * t
-
+        const = 1 / np.sqrt(1 + t**2)
+        s = const * t
         R = np.eye(n)
-        R[p, p] = c
+        R[p, p] = const
         R[p, q] = -s
         R[q, p] = s
-        R[q, q] = c
-        
+        R[q, q] = const
         A = np.dot(R.T, np.dot(A, R))
         U = np.dot(U, R)
-        
         k += 1
     
     return A, U
 
-
-def calculate_eigenvalues(A_init):
-    A, U = jacobi(A_init)  # Apelăm funcția Jacobi pentru a obține matricea diagonalizată și matricea de transformare
-    Lambda = np.diag(np.diag(A))  # Valorile proprii sunt pe diagonala matricei A
-    return Lambda
-
-def verify_eigenvalues(A_init, U, Lambda):
-    approx = np.dot(A_init, U)
-    exact = np.dot(U, Lambda)
-    norm = np.linalg.norm(approx - exact)
+def verifEigenvaluesNorm(A_init, U, Lambda):
+    approximation = np.dot(A_init, U)
+    rez = np.dot(U, Lambda)
+    norm = np.linalg.norm(approximation - rez)
     print("Norma matrice ||A_init * U - U * Lambda||:", norm)
 
 
-def cholesky_iterative(A, epsilon=1e-10, max_iter=10000):
+def choleskyMethod(A, eps=1e-10, K_max=10000):
     n = A.shape[0]
     L = np.zeros_like(A, dtype=float)
     
@@ -66,63 +56,62 @@ def cholesky_iterative(A, epsilon=1e-10, max_iter=10000):
                 L[i][j] = (1.0 / L[j][j]) * (A[i][j] - temp_sum)
     
     L = np.tril(L)
-    
-    A_new = np.dot(L, L.T)
+    A_Mat = np.dot(L, L.T)
     
     k = 1
-    while k < max_iter:
+    while k < K_max:
         for i in range(n):
             for j in range(i + 1):
                 temp_sum = np.sum(L[i][k] * L[j][k] for k in range(j))
                 if i == j:
-                    L[i][j] = np.sqrt(A_new[i][i] - temp_sum)
+                    L[i][j] = np.sqrt(A_Mat[i][i] - temp_sum)
                 else:
-                    L[i][j] = (1.0 / L[j][j]) * (A_new[i][j] - temp_sum)
+                    L[i][j] = (1.0 / L[j][j]) * (A_Mat[i][j] - temp_sum)
 
-        A_new = np.dot(L, L.T)
-
-        difference = np.linalg.norm(A_new - A)
-        if difference < epsilon:
-            break
-        
+        A_Mat = np.dot(L, L.T)
+        norm = np.linalg.norm(A_Mat - A)
+        if norm < eps:
+            break 
         k += 1
     
-    return A_new
+    return A_Mat
 
 p = 5
 n = 5
-epsilon = 1e-10
+eps = 1e-10
+
 A_init = np.random.rand(p, n)
 A_init = np.triu(A_init) + np.triu(A_init, 1).T
 A_init = np.dot(A_init.T, A_init)
-A, U = jacobi(A_init, epsilon=epsilon)
-Lambda = calculate_eigenvalues(A_init) 
-print('Jacobi : ',A,U)
-verify_eigenvalues(A_init, U, Lambda)
+A, U = jacobiDivision(A_init, eps=eps)
 
-# Exemplu de utilizare
+Lambda = np.diag(np.diag(A)) 
+print(f"Jacobi Matrice : \n {A} \n {U}")
+verifEigenvaluesNorm(A_init, U, Lambda)
+
 p = 5
 n = 5
-epsilon = 1e-10
+eps = 1e-10
 A = np.random.rand(p, n)
 A = np.dot(A.T, A) 
-A_final = cholesky_iterative(A, epsilon=epsilon)
-print("A_final - Choleski:")
-print(A_final)
+A_final = choleskyMethod(A, eps=eps)
+print(f" Choleski Matrice : \n  {A_final}")
 
 
 U, S, VT = np.linalg.svd(A)
-singular_values = S
+val_singulare = S
 rank = np.linalg.matrix_rank(A)
 condition_number = np.linalg.cond(A)
-pseudoinv = np.dot(VT.T, np.dot(np.diag(1/S), U.T))
-pseudo_least_squares = np.dot(np.linalg.inv(np.dot(A.T, A)), A.T)
+MoorePenInv = np.dot(VT.T, np.dot(np.diag(1/S), U.T))
+LSC = np.dot(np.linalg.inv(np.dot(A.T, A)), A.T)
 
-print("Valorile singulare ale matricei A:")
-print(singular_values)
-print("Rangul matricei A:", rank)
-print("Nr de condiționare al matricei A:", condition_number)
-print("Pseudoinversa Moore-Penrose a matricei A:")
-print(pseudoinv)
-norm_pseudo_diff = np.linalg.norm(pseudoinv - pseudo_least_squares, ord=1)
-print("Norma ||AI - AJ||1:", norm_pseudo_diff)
+print(f"Valorile singulare ale matricei A : \n {val_singulare}")
+
+print(f"Rangul matricei A: \n {rank}")
+
+print(f"Nr de conditionare :  {condition_number}")
+
+print(f"Pseudoinversa Moore-Penrose a matricei A : \n {MoorePenInv}")
+
+norm_pseudo_diff = np.linalg.norm(MoorePenInv - LSC, ord=1)
+print(f"Norma ||AI - AJ|| : {norm_pseudo_diff}")
